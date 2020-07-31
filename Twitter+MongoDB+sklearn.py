092e389989e93dc76b2cf175636a6f332db23c04
João Paulo Marques Oliveira <58232319+joaopaulomarquesoliveira@users.noreply.github.com>
2020-07-30 23:10:54 -0300

    Update Twitter+MongoDB+sklearn.py

--- Twitter+MongoDB+sklearn.py
+++ Twitter+MongoDB+sklearn.py
@@ -1,51 +1,36 @@
-from tweepy import Stream
-from tweepy import OAuthHandler
+from tweepy import Stream                   # tweepy import Stream and StreamListener because they are receive tweets in real time / Porque recebem tweets em tempo real
+from tweepy import OAuthHandler             #for twitter authentication / para autenticação no twitter
 from tweepy.streaming import StreamListener
-from pymongo import MongoClient
+from pymongo import MongoClient             #Connector Python/MongoDB
+import pandas as pd
 import json
 
 
 #consumer key, consumer secret, access token, access secret.
+#it's possible to get the keys, opening a developer account in https://developer.twitter.com/
+#É possível obter as chaves, abrindo uma conta de desenvolvedor em https://developer.twitter.com/
+
 ckey="7U1Mb2xh4JuZqFvLXxXQ0NpdK"
 csecret="gY1UF2DnZoqYkFQEKVfVZerbNev4GTao08MI3d0UZw8RFVFOpl"
 atoken="342377408-li0bFQ5dcIwrRfkUH4PSFvq0c7JhmHUoNkKIEypz"
 asecret="9HxwWdbTGWXnsyDyITV5RaUAglACLIdfQtiujKHWiz2oI"
 
 class listener(StreamListener):
-
     def on_data(self, status):
-        a=json.loads(status)
-        
-        created_at = a["created_at"]
-        id_str=a["id_str"]
-
-        text= a["text"]
+        twitter= json.loads(status)        
+        created_at= twitter["created_at"]
+        id_str= twitter["id_str"]
+        text= twitter["text"]
         obj={"created_at": created_at,"id_str":id_str,"text":text}
         twitterid= col.insert_one(obj).inserted_id
-
         print (obj)
         
-
-
-
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
-
-
-
-
 twitterStream = Stream(auth, listener())
 
-
-
 conn=MongoClient('localhost',27017)
-
-
-
 db=conn.twitterdb
-
-
-
 col=db.tweets
 
 
@@ -56,19 +41,11 @@ twitterStream.filter(track=["python"])
 twitterStream.disconnect()
 
 
-
-
-(col.find_one())
-
-
-
-
 dataset=[{'created_at': item["created_at"],'text': item["text"],} for item in col.find()]
 
 
 
 
-import pandas as pd
 
 
 
